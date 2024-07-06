6cec17b669dd2dd6da64c7145103b2701ec1875a
suyogpatil1161 <165510313+suyogpatil1161@users.noreply.github.com>
2024-07-06 19:57:05 +0530

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -2,10 +2,10 @@ package training.taylor.timetracker.core;
 
 import static org.junit.Assert.*;
 
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
